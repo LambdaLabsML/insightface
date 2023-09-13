ce7de9f5e7411ebf117170426481e389117ab698
chuanli11 <cl.chuanli@gmail.com>
2023-09-12 21:38:40 -0700

    test training script

--- examples/chimp/trainer_synthetics.py
+++ examples/chimp/trainer_synthetics.py
@@ -18,9 +18,9 @@ class FaceSynthetics(pl.LightningModule):
     def __init__(self, backbone):
         super().__init__()
         self.save_hyperparameters()
-        backbone = timm.create_model(backbone, num_classes=68*2)
+        backbone = timm.create_model(backbone, num_classes=68 * 2)
         self.backbone = backbone
-        self.loss = nn.L1Loss(reduction='mean')
+        self.loss = nn.L1Loss(reduction="mean")
         self.hard_mining = False
 
     def forward(self, x):
@@ -32,44 +32,49 @@ class FaceSynthetics(pl.LightningModule):
         x, y = batch
         y_hat = self.backbone(x)
         if self.hard_mining:
-            loss = torch.abs(y_hat - y) #(B,K)
-            loss = torch.mean(loss, dim=1) #(B,)
+            loss = torch.abs(y_hat - y)  # (B,K)
+            loss = torch.mean(loss, dim=1)  # (B,)
             B = len(loss)
-            S = int(B*0.5)
+            S = int(B * 0.5)
             loss, _ = torch.sort(loss, descending=True)
             loss = loss[:S]
             loss = torch.mean(loss) * 5.0
         else:
             loss = self.loss(y_hat, y) * 5.0
-        self.log('train_loss', loss, on_epoch=True)
+        self.log("train_loss", loss, on_epoch=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.backbone(x)
         loss = self.loss(y_hat, y)
-        self.log('val_loss', loss, on_step=True)
+        self.log("val_loss", loss, on_step=True)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.backbone(x)
         loss = self.loss(y_hat, y)
-        self.log('test_loss', loss)
+        self.log("test_loss", loss)
 
     def configure_optimizers(self):
-        #return torch.optim.Adam(self.parameters(), lr=0.0002)
-        opt = torch.optim.SGD(self.parameters(), lr = 0.1, momentum=0.9, weight_decay = 0.0005)
+        # return torch.optim.Adam(self.parameters(), lr=0.0002)
+        opt = torch.optim.SGD(
+            self.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0005
+        )
+
         def lr_step_func(epoch):
             return 0.1 ** len([m for m in [15, 25, 28] if m <= epoch])
-        scheduler = torch.optim.lr_scheduler.LambdaLR(
-                optimizer=opt, lr_lambda=lr_step_func)
-        lr_scheduler = {
-                'scheduler': scheduler,
-                'name': 'learning_rate',
-                'interval':'epoch',
-                'frequency': 1}
-        return [opt], [lr_scheduler]
 
+        scheduler = torch.optim.lr_scheduler.LambdaLR(
+            optimizer=opt, lr_lambda=lr_step_func
+        )
+        lr_scheduler = {
+            "scheduler": scheduler,
+            "name": "learning_rate",
+            "interval": "epoch",
+            "frequency": 1,
+        }
+        return [opt], [lr_scheduler]
 
 
 def cli_main():
@@ -79,11 +84,16 @@ def cli_main():
     # args
     # ------------
     parser = ArgumentParser()
-    parser.add_argument('--backbone', default='resnet50d', type=str)
-    parser.add_argument('--batch_size', default=64, type=int)
-    parser.add_argument('--root', default='data/synthetics', type=str)
-    parser.add_argument('--num-gpus', default=2, type=int)
-    parser.add_argument('--tf32', action='store_true')
+    parser.add_argument("--backbone", default="resnet50d", type=str)
+    parser.add_argument("--batch_size", default=64, type=int)
+    parser.add_argument("--root", default="data/synthetics", type=str)
+    parser.add_argument("--num-gpus", default=2, type=int)
+    parser.add_argument("--tf32", action="store_true")
+    parser.add_argument(
+        "--pre-trained-path",
+        type=str,
+        default="/media/ubuntu/home1/deepvoodoo/Chimp/models/synthetic_resnet50d.ckpt",
+    )
     parser = pl.Trainer.add_argparse_args(parser)
     args = parser.parse_args()
 
@@ -101,14 +111,23 @@ def cli_main():
     train_set = FaceDataset(root_dir=args.root, is_train=True)
     val_set = FaceDataset(root_dir=args.root, is_train=False)
 
-    train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=3, pin_memory=True)
+    train_loader = DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=3,
+        pin_memory=True,
+    )
     val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False)
 
     # ------------
     # model
     # ------------
-    model = FaceSynthetics(backbone=args.backbone)
-    ckpt_path = 'work_dirs/synthetics'
+    # model = FaceSynthetics(backbone=args.backbone)
+
+    model = FaceSynthetics.load_from_checkpoint(args.pre_trained_path).cuda()
+
+    ckpt_path = "work_dirs/synthetics"
     if not os.path.exists(ckpt_path):
         os.makedirs(ckpt_path)
 
@@ -116,25 +135,25 @@ def cli_main():
     # training
     # ------------
     checkpoint_callback = ModelCheckpoint(
-            monitor='val_loss',
-            dirpath=ckpt_path,
-            filename='{epoch:02d}-{val_loss:.6f}',
-            save_top_k=10,
-            mode='min',
-            )
-    lr_monitor = LearningRateMonitor(logging_interval='step')
+        monitor="val_loss",
+        dirpath=ckpt_path,
+        filename="{epoch:02d}-{val_loss:.6f}",
+        save_top_k=10,
+        mode="min",
+    )
+    lr_monitor = LearningRateMonitor(logging_interval="step")
     trainer = pl.Trainer(
-        gpus = args.num_gpus,
-        accelerator="ddp",
+        gpus=args.num_gpus,
+        accelerator="cuda",
         benchmark=True,
-		logger=TensorBoardLogger(osp.join(ckpt_path, 'logs')),
+        logger=TensorBoardLogger(osp.join(ckpt_path, "logs")),
         callbacks=[checkpoint_callback, lr_monitor],
         check_val_every_n_epoch=1,
-        progress_bar_refresh_rate=1,
+        # progress_bar_refresh_rate=1,
         max_epochs=30,
     )
     trainer.fit(model, train_loader, val_loader)
 
-if __name__ == '__main__':
-    cli_main()
 
+if __name__ == "__main__":
+    cli_main()
